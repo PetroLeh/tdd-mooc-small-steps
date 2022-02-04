8ab8ef33a5866b582a4a4ef7bcce6762bec45ee9
Petro Lehtonen <petro.lehtonen@gmail.com>
2022-02-04 10:40:32 +0200

    refactoring

--- src/prices.mjs
+++ src/prices.mjs
@@ -26,7 +26,7 @@ function createApp(database) {
 
   function parseDate(dateString) {
     if (dateString) {
-      return new Date(dateString);
+        return Temporal.PlainDate.from(dateString);
     }
   }
 
@@ -77,21 +77,18 @@ function createApp(database) {
   }
 
   function isMonday(date) {
-      date = date.toTemporalInstant()
-                 .toZonedDateTimeISO("UTC")
-                 .toPlainDate();
       return date.dayOfWeek == 1;
   }
 
   function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let holiday = new Date(row.holiday);
+      let holiday = Temporal.PlainDate.from(row.holiday)
       if (
         date &&
-        date.getFullYear() === holiday.getFullYear() &&
-        date.getMonth() === holiday.getMonth() &&
-        date.getDate() === holiday.getDate()
+        date.year === holiday.year &&
+        date.month === holiday.month &&
+        date.day === holiday.day
       ) {
         return true;
       }
